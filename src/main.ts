d1000da03994695d13d65d7e88edd9968977507f
fleish80 <Fleish.D@gmail.com>
2023-09-11 08:53:29 +0300

    analog

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,8 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { routes } from '@analogjs/router';
+
+console.log(routes);
 
 bootstrapApplication(AppComponent, appConfig);
